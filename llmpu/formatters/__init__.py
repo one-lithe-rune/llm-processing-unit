3adf8dabaab22e7673f966a585071a67f53e6b27
Stefan Kapusniak <121311569+one-lithe-rune@users.noreply.github.com>
2024-05-18 14:43:46 +0100

    Improve importability of the library into external projects (#1)
    * Add project/build configuration
    
    - Add an initial attempt at a project/build configuration
    to pyproject.toml
    
    * Fix naming of Llama3 formatters
    
    - Correct naming of Llama3 formatters
    - Make all session formatters available from the command
    line helpers

--- llmpu/formatters/__init__.py
+++ llmpu/formatters/__init__.py
@@ -2,8 +2,12 @@ from .base import BaseSessionFormatter
 from .format_alpaca import AlpacaSessionFormatter
 from .format_llama3 import (
     Llama3SessionFormatter,
-    Llama3InstructSessionFormater,
+    Llama3InstructSessionFormatter,
     Llama3ChatSessionFormatter,
 )
 from .format_oaichat import OAIChatSessionFormatter
-from .args import from_args, add_args
+from .args import (
+    from_args,
+    add_args,
+    session_formatters,
+)
